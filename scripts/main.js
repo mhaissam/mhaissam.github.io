8b7fae4916931d6b0adc578bd8343fa8a972e362
mhaissam <74671278+mhaissam@users.noreply.github.com>
2020-11-18 17:56:25 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+
+        var mySrc = myImage.getAttribute('src');
+        if (mySrc === 'bilder/firefox.png') {
+            myImage.setAttribute('src','bilder/firefox2.png');
+        } else {
+          myImage.setAttribute('src','bilder/firefox.png');
+          }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Bitte geben Sie Ihren Namen ein.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla ist cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla ist cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
